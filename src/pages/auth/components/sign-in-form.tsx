fc86b50403ced013a6ee74112a4f7f116f7953cc
lonejavid <60721040+lonejavid@users.noreply.github.com>
2025-07-22 20:50:48 +0530

    make it better

--- src/pages/auth/components/sign-in-form.tsx
+++ src/pages/auth/components/sign-in-form.tsx
@@ -1,8 +1,7 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { Calendar, Command, ArrowRight } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { Calendar, ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
