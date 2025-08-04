3c5898e07f7277120efd9063c10f6931717afc71
lonejavid <60721040+lonejavid@users.noreply.github.com>
2025-08-04 13:16:08 +0530

    .env chaged

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import {
   authenticationRoutePaths,
   protectedRoutePaths,
@@ -45,7 +45,8 @@ function AppRoutes() {
         </Route>
 
         {/* Catch-all for undefined routes */}
-        <Route path="*" element={<>page not     kjhghjklkjhgvcxcfg found</>} />
+        {/* <Route path="*" element={<>page not     kjhghjklkjhgvcxcfg found</>} /> */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
