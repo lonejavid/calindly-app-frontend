69257a1e6de9f9c93df788689bb8e06995973d62
lonejavid <60721040+lonejavid@users.noreply.github.com>
2025-08-30 15:08:14 +0530

    added

--- src/pages/external_page/_components/booking-form.tsx
+++ src/pages/external_page/_components/booking-form.tsx
@@ -494,7 +494,7 @@ const BookingForm = (props: { event: Event }) => {
       const payload = {
         guestName: values.guestName,
         guestEmail: values.guestEmail,
-         additionalInfo: `${values.additionalInfo || ""} (+selectedTime: ${selectedSlot}  selectedDate: ${selectedDate} duration: ${event.duration}}+)`,
+         additionalInfo: `${values.additionalInfo || ""} (+selectedTime: ${selectedSlot} eventDescription:${event.description} selectedDate: ${selectedDate} duration: ${event.duration}}+)`,
         ...(event.allowGuests && values.guestEmails && { guestEmails: values.guestEmails }),
         eventId: event.id,
         startTime: startTimeUTC.toISOString(),
