3b8187d278037778846e4d690ec7096a12c89474
lonejavid <60721040+lonejavid@users.noreply.github.com>
2025-08-31 19:30:07 +0530

    added

--- src/pages/meeting/index.tsx
+++ src/pages/meeting/index.tsx
@@ -16,7 +16,7 @@ const Meetings = () => {
     queryKey: ["userMeetings", period],
     queryFn: () => getUserMeetingsQueryFn(period),
   });
-console.log("user Meetings are here ",data);
+console.log("user timezone is here  ",data?.timezone);
   const meetings = data?.meetings || [];
 
   return (
