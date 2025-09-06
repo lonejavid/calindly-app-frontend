3a307aa82732f860c02fa6b904ed9963a1e47424
lonejavid <60721040+lonejavid@users.noreply.github.com>
2025-09-06 19:21:32 +0530

    timezone conversion

--- src/routes/authRoute.tsx
+++ src/routes/authRoute.tsx
@@ -1,3 +1,17 @@
+// import { useStore } from "@/store/store";
+// import { Navigate, Outlet } from "react-router-dom";
+// import { PROTECTED_ROUTES } from "./common/routePaths";
+
+// const AuthRoute = () => {
+//   const { accessToken, user } = useStore();
+
+//   if (!accessToken && !user) return <Outlet />;
+
+//   return <Navigate to={PROTECTED_ROUTES.EVENT_TYPES} replace />;
+// };
+
+// export default AuthRoute;
+
 import { useStore } from "@/store/store";
 import { Navigate, Outlet } from "react-router-dom";
 import { PROTECTED_ROUTES } from "./common/routePaths";
@@ -5,8 +19,15 @@ import { PROTECTED_ROUTES } from "./common/routePaths";
 const AuthRoute = () => {
   const { accessToken, user } = useStore();
 
+  // If no access token, show auth pages
   if (!accessToken && !user) return <Outlet />;
+  
+  // If user is not approved, redirect to setup
+  if (user && !user.isApproved) {
+    return <Navigate to={PROTECTED_ROUTES.SETUP} replace />;
+  }
 
+  // For approved users, redirect to event types
   return <Navigate to={PROTECTED_ROUTES.EVENT_TYPES} replace />;
 };
 
