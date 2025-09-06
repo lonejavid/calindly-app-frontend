4412207136cf668379f6bfbdbc4e46eb980cb499
lonejavid <60721040+lonejavid@users.noreply.github.com>
2025-09-06 19:23:02 +0530

    timezone conversion

--- src/routes/authRoute.tsx
+++ src/routes/authRoute.tsx
@@ -1,17 +1,3 @@
-// import { useStore } from "@/store/store";
-// import { Navigate, Outlet } from "react-router-dom";
-// import { PROTECTED_ROUTES } from "./common/routePaths";
-
-// const AuthRoute = () => {
-//   const { accessToken, user } = useStore();
-
-//   if (!accessToken && !user) return <Outlet />;
-
-//   return <Navigate to={PROTECTED_ROUTES.EVENT_TYPES} replace />;
-// };
-
-// export default AuthRoute;
-
 import { useStore } from "@/store/store";
 import { Navigate, Outlet } from "react-router-dom";
 import { PROTECTED_ROUTES } from "./common/routePaths";
@@ -19,15 +5,8 @@ import { PROTECTED_ROUTES } from "./common/routePaths";
 const AuthRoute = () => {
   const { accessToken, user } = useStore();
 
-  // If no access token, show auth pages
   if (!accessToken && !user) return <Outlet />;
-  
-  // If user is not approved, redirect to setup
-  if (user && !user.isApproved) {
-    return <Navigate to={PROTECTED_ROUTES.SETUP} replace />;
-  }
 
-  // For approved users, redirect to event types
   return <Navigate to={PROTECTED_ROUTES.EVENT_TYPES} replace />;
 };
 
--- src/routes/protectedRoute.tsx
+++ src/routes/protectedRoute.tsx
@@ -1,32 +1,12 @@
-// import { Navigate, Outlet } from "react-router-dom";
-// import { useStore } from "@/store/store";
-
-// const ProtectedRoute = () => {
-//   const { accessToken } = useStore();
-
-//   if (accessToken) return <Outlet />;
-
-//   return <Navigate to="/" replace />;
-// };
-
-// export default ProtectedRoute;
-
 import { Navigate, Outlet } from "react-router-dom";
 import { useStore } from "@/store/store";
 
 const ProtectedRoute = () => {
-  const { accessToken, user } = useStore();
+  const { accessToken } = useStore();
 
-  // If no access token, redirect to login
-  if (!accessToken) return <Navigate to="/" replace />;
-  
-  // If user exists but is not approved, allow access to setup page only
-  if (user && !user.isApproved) {
-    return window.location.pathname === "/app/setup" ? <Outlet /> : <Navigate to="/app/setup" replace />;
-  }
+  if (accessToken) return <Outlet />;
 
-  // For approved users with access token, allow access to all protected routes
-  return <Outlet />;
+  return <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
