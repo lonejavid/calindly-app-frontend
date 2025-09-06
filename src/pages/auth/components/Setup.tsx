9982a3f19999249cf1dfd31c8c35c4da4d8e3998
lonejavid <60721040+lonejavid@users.noreply.github.com>
2025-09-06 18:12:08 +0530

    timezone conversion

---
+++ src/pages/auth/components/Setup.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const Setup = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">👋 Hello, Setup!</h1>
+    </div>
+  );
+};
+
+export default Setup;
