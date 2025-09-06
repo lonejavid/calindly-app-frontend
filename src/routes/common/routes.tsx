fb68fcca93d53742f12140dc284822385a2b1307
lonejavid <60721040+lonejavid@users.noreply.github.com>
2025-09-06 20:05:02 +0530

    timezone conversion

--- src/routes/common/routes.tsx
+++ src/routes/common/routes.tsx
@@ -1,41 +1,3 @@
-// import { AUTH_ROUTES, PROTECTED_ROUTES, PUBLIC_ROUTES } from "./routePaths";
-// import SignIn from "@/pages/auth/signin";
-// import SignUp from "@/pages/auth/signup";
-// import EventType from "@/pages/event_type";
-// import Meetings from "@/pages/meeting";
-// import Availability from "@/pages/availability";
-// import Integrations from "@/pages/integrations";
-// // import UserEventsPage from "@/pages/external_page/user-events";
-// import UserSingleEventPage from "@/pages/external_page/user-single-event";
-// import ScheduleyLanding from "@/pages/landing/ScheduleyLanding";
-// import OAuthSuccess from "@/pages/GoogleAothPages/OAuthSuccess";
-// import TermsOfService from "@/pages/Policy/TermsOfService";
-// import PrivacyPolicy from "@/pages/Policy/PrivacyPolicy";
-// import CareersPage from "@/pages/landing/CareersPage";
-// import Setup from "@/pages/auth/components/Setup";
-// export const authenticationRoutePaths = [
-//    { path: "/", element: <ScheduleyLanding /> },
-//    {path:"/terms",element:<TermsOfService/>},
-//    {path:"/privacy",element:<PrivacyPolicy/>},
-//     {path:"/carrer",element:<CareersPage/>},
-//   { path: AUTH_ROUTES.SIGN_IN, element: <SignIn /> },
-//   { path: AUTH_ROUTES.SIGN_UP, element: <SignUp /> },
-// ];
-
-// export const protectedRoutePaths = [
-//   { path: PROTECTED_ROUTES.SETUP, element: <Setup/> },
-//   { path: PROTECTED_ROUTES.EVENT_TYPES, element: <EventType /> },
-//   { path: PROTECTED_ROUTES.MEETINGS, element: <Meetings /> },
-//   { path: PROTECTED_ROUTES.AVAILBILITIY, element: <Availability /> },
-//   { path: PROTECTED_ROUTES.INTEGRATIONS, element: <Integrations /> },
-// ];
-
-// export const publicRoutePaths = [
-//   // { path: PUBLIC_ROUTES.USER_EVENTS, element: <UserEventsPage /> },
-//   { path: PUBLIC_ROUTES.USER_SINGLE_EVENT, element: <UserSingleEventPage /> },
-//   { path: PUBLIC_ROUTES.OAUTH_SUCCESS, element: <OAuthSuccess /> },
-// ];
-
 import { AUTH_ROUTES, PROTECTED_ROUTES, PUBLIC_ROUTES } from "./routePaths";
 import SignIn from "@/pages/auth/signin";
 import SignUp from "@/pages/auth/signup";
@@ -43,25 +5,23 @@ import EventType from "@/pages/event_type";
 import Meetings from "@/pages/meeting";
 import Availability from "@/pages/availability";
 import Integrations from "@/pages/integrations";
+// import UserEventsPage from "@/pages/external_page/user-events";
 import UserSingleEventPage from "@/pages/external_page/user-single-event";
 import ScheduleyLanding from "@/pages/landing/ScheduleyLanding";
 import OAuthSuccess from "@/pages/GoogleAothPages/OAuthSuccess";
 import TermsOfService from "@/pages/Policy/TermsOfService";
 import PrivacyPolicy from "@/pages/Policy/PrivacyPolicy";
 import CareersPage from "@/pages/landing/CareersPage";
-import Setup from "@/pages/auth/components/Setup";
-
 export const authenticationRoutePaths = [
-  { path: "/", element: <ScheduleyLanding /> },
-  { path: "/terms", element: <TermsOfService /> },
-  { path: "/privacy", element: <PrivacyPolicy /> },
-  { path: "/carrer", element: <CareersPage /> },
+   { path: "/", element: <ScheduleyLanding /> },
+   {path:"/terms",element:<TermsOfService/>},
+   {path:"/privacy",element:<PrivacyPolicy/>},
+    {path:"/carrer",element:<CareersPage/>},
   { path: AUTH_ROUTES.SIGN_IN, element: <SignIn /> },
   { path: AUTH_ROUTES.SIGN_UP, element: <SignUp /> },
 ];
 
 export const protectedRoutePaths = [
-  { path: PROTECTED_ROUTES.SETUP, element: <Setup /> },
   { path: PROTECTED_ROUTES.EVENT_TYPES, element: <EventType /> },
   { path: PROTECTED_ROUTES.MEETINGS, element: <Meetings /> },
   { path: PROTECTED_ROUTES.AVAILBILITIY, element: <Availability /> },
@@ -69,6 +29,7 @@ export const protectedRoutePaths = [
 ];
 
 export const publicRoutePaths = [
+  // { path: PUBLIC_ROUTES.USER_EVENTS, element: <UserEventsPage /> },
   { path: PUBLIC_ROUTES.USER_SINGLE_EVENT, element: <UserSingleEventPage /> },
   { path: PUBLIC_ROUTES.OAUTH_SUCCESS, element: <OAuthSuccess /> },
 ];
--- src/routes/protectedRoute.tsx
+++ src/routes/protectedRoute.tsx
@@ -1,26 +1,12 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useStore } from "@/store/store";
-import { PROTECTED_ROUTES } from "./common/routePaths";
-
-// const ProtectedRoute = () => {
-//   const { accessToken } = useStore();
-
-//   if (accessToken) return <Outlet />;
-
-//   return <Navigate to="/" replace />;
-// };
 
 const ProtectedRoute = () => {
-  const { accessToken, user } = useStore();
+  const { accessToken } = useStore();
 
-  if (!accessToken) return <Navigate to="/" replace />;
-  
-  // If user is not approved and trying to access non-setup pages
-  if (user && !user.isApproved && !window.location.pathname.includes('/setup')) {
-    return <Navigate to={PROTECTED_ROUTES.SETUP} replace />;
-  }
+  if (accessToken) return <Outlet />;
 
-  return <Outlet />;
+  return <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
