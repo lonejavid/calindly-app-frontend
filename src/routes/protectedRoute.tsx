66bf7fd1247fba641127cdf3a914e5f7d39f0b65
lonejavid <60721040+lonejavid@users.noreply.github.com>
2025-09-06 19:40:53 +0530

    timezone conversion

--- src/routes/protectedRoute.tsx
+++ src/routes/protectedRoute.tsx
@@ -1,37 +1,12 @@
-// import { Navigate, Outlet } from "react-router-dom";
-// import { useStore } from "@/store/store";
-
-// const ProtectedRoute = () => {
-//   const { accessToken } = useStore();
-
-//   if (accessToken) return <Outlet />;
-
-//   return <Navigate to="/" replace />;
-// };
-
-// export default ProtectedRoute;
-
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useStore } from "@/store/store";
 
 const ProtectedRoute = () => {
-  const { accessToken, user } = useStore();
-  const location = useLocation();
+  const { accessToken } = useStore();
 
-  if (!accessToken) return <Navigate to="/" replace />;
+  if (accessToken) return <Outlet />;
 
-  // Allow access to setup page for unapproved users
-  if (location.pathname === "/app/setup") {
-    return <Outlet />;
-  }
-
-  // For all other protected routes, user must be approved
-  if (user && user.isApproved) {
-    return <Outlet />;
-  }
-
-  // If user is not approved and trying to access other protected routes
-  return <Navigate to="/app/setup" replace />;
+  return <Navigate to="/" replace />;
 };
 
 export default ProtectedRoute;
