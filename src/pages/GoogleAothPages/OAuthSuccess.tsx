16c218c497971677c7fca90e90618dce5a32ef45
lonejavid <60721040+lonejavid@users.noreply.github.com>
2025-09-06 19:45:11 +0530

    timezone conversion

--- src/pages/GoogleAothPages/OAuthSuccess.tsx
+++ src/pages/GoogleAothPages/OAuthSuccess.tsx
@@ -1,6 +1,57 @@
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useStore } from "@/store/store"; // ✅ import your Zustand store
+
+// const OAuthSuccess = () => {
+//   const navigate = useNavigate();
+//   const setUser = useStore((state) => state.setUser);
+//   const setAccessToken = useStore((state) => state.setAccessToken);
+
+//   useEffect(() => {
+//     const queryParams = new URLSearchParams(window.location.search);
+//     const accessToken = queryParams.get("accessToken");
+//     const userJson = queryParams.get("user");
+
+//     if (accessToken && userJson) {
+//       try {
+//         const user = JSON.parse(decodeURIComponent(userJson));
+//         console.log("this is the user which is with approved satatus ",user);
+
+        
+//         setUser(user);
+//         setAccessToken(accessToken);
+
+//         // Optional: Save to localStorage for persistence
+//         localStorage.setItem("accessToken", accessToken);
+//         localStorage.setItem("user", JSON.stringify(user));
+
+//         // navigate("/app/event_types");
+//           if (user.isApproved) {
+//           navigate("/app/event_types");
+//         } else {
+//           navigate("/app/setup");
+//         }
+//       } catch (err) {
+//         console.error("❌ Failed to parse or store data", err);
+//       }
+//     } else {
+//       console.error("❌ Missing token or user data");
+//     }
+//   }, [navigate, setUser, setAccessToken]);
+
+//   return (
+//     <div className="text-center text-white mt-20 text-xl">
+//       Redirecting, please wait...
+//     </div>
+//   );
+// };
+
+// export default OAuthSuccess;
+
+
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useStore } from "@/store/store"; // ✅ import your Zustand store
+import { useStore } from "@/store/store";
 
 const OAuthSuccess = () => {
   const navigate = useNavigate();
@@ -15,8 +66,6 @@ const OAuthSuccess = () => {
     if (accessToken && userJson) {
       try {
         const user = JSON.parse(decodeURIComponent(userJson));
-        console.log("this is the user which is with approved satatus ",user);
-
         
         setUser(user);
         setAccessToken(accessToken);
@@ -25,8 +74,8 @@ const OAuthSuccess = () => {
         localStorage.setItem("accessToken", accessToken);
         localStorage.setItem("user", JSON.stringify(user));
 
-        // navigate("/app/event_types");
-          if (user.isApproved) {
+        // Redirect based on approval status
+        if (user.isApproved) {
           navigate("/app/event_types");
         } else {
           navigate("/app/setup");
